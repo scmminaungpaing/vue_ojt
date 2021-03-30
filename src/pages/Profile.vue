2fb906a3035e48b629afdc06724cbf9e9c8ec519
scmminaungpaing <scm.minaungpaing@gmail.com>
2021-03-30 17:05:02 +0630

    add edit profile

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -10,6 +10,7 @@
             alt="user"
             width="100%"
           />
+          <button class="btn btn-sm btn-info mt-2 py-0 w-100" data-toggle="modal" data-target="#profileModal"><i class="fas fa-edit"></i> Edit Profile Image</button>
         </div>
         <div class="col-sm-10 d-flex justify-content-between">
           <div class="px-3 pt-5">
@@ -54,7 +55,7 @@
             <router-link :to="{name: 'post-edit' , params: { id: post.id, post:post } }" class="btn btn-info btn-sm">
               <i class="fas fa-pencil-alt"></i>
             </router-link>
-            <button class="btn btn-danger btn-sm ml-2"><i class="fa fa-times"></i></button>
+            <button @click="deletePost(post.id)"  class="btn btn-danger btn-sm ml-2"><i class="fa fa-times"></i></button>
           </div>
         </div>
         <div class="card-body">
@@ -63,135 +64,157 @@
       </div>
 
       <!-- Modal -->
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-user"></i> <b>Edit Profile!</b></h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body pt-5 pb-4 px-4">
-            <form @submit.prevent="updateProfile(user.id)" @keydown="form.onKeydown($event)">
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="row">
-                      <div class="col-sm-3 text-right">
-                        <label for="title"><b>Username :</b></label>
-                      </div>
-                      <div class="col-sm-8">
-                        <input
-                          v-model="form.name"
-                          type="text"
-                          name="name"
-                          placeholder="Enter username"
-                          class="form-control"
-                          :class="{ 'is-invalid': form.errors.has('name') }"
-                        />
-                        <has-error :form="form" field="name"></has-error>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="row">
-                      <div class="col-sm-3 text-right">
-                        <label for="title"><b>E-mail :</b></label>
-                      </div>
-                      <div class="col-sm-8">
-                        <input
-                          v-model="form.email"
-                          type="email"
-                          name="email"
-                          placeholder="Enter your email"
-                          class="form-control"
-                          :class="{ 'is-invalid': form.errors.has('email') }"
-                        />
-                        <has-error :form="form" field="email"></has-error>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="row">
-                      <div class="col-sm-3 text-right">
-                        <label for="phone"><b>Phone :</b></label>
-                      </div>
-                      <div class="col-sm-8">
-                        <input
-                          v-model="form.phone"
-                          type="text"
-                          name="phone"
-                          placeholder="Enter Phone number"
-                          class="form-control"
-                          :class="{ 'is-invalid': form.errors.has('phone') }"
-                        />
-                        <has-error :form="form" field="phone"></has-error>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="row">
-                      <div class="col-sm-3 text-right">
-                        <label for="dob"><b>Date of Birth :</b></label>
-                      </div>
-                      <div class="col-sm-8">
-                        <input
-                          v-model="form.dob"
-                          type="date"
-                          name="dob"
-                          class="form-control"
-                          :class="{ 'is-invalid': form.errors.has('dob') }"
-                        />
-                        <has-error :form="form" field="dob"></has-error>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="row">
-                  <div class="col-sm-12">
-                    <div class="row">
-                      <div class="col-sm-3 text-right">
-                        <label for="address"><b>Address :</b></label>
-                      </div>
-                      <div class="col-sm-8">
-                        <input
-                          v-model="form.address"
-                          type="text"
-                          name="address"
-                          placeholder="Enter Address"
-                          class="form-control"
-                          :class="{ 'is-invalid': form.errors.has('address') }"
-                        />
-                        <has-error :form="form" field="address"></has-error>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              
-              <button :disabled="form.busy" type="submit" class="btn btn-primary w-100 mt-3">
-                Update Profile
+      <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-user"></i> <b>Edit Profile!</b></h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
               </button>
-            </form>
+            </div>
+            <div class="modal-body pt-5 pb-4 px-4">
+              <form @submit.prevent="updateProfile(user.id)" @keydown="form.onKeydown($event)">
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="row">
+                        <div class="col-sm-3 text-right">
+                          <label for="title"><b>Username :</b></label>
+                        </div>
+                        <div class="col-sm-8">
+                          <input
+                            v-model="form.name"
+                            type="text"
+                            name="name"
+                            placeholder="Enter username"
+                            class="form-control"
+                            :class="{ 'is-invalid': form.errors.has('name') }"
+                          />
+                          <has-error :form="form" field="name"></has-error>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="row">
+                        <div class="col-sm-3 text-right">
+                          <label for="title"><b>E-mail :</b></label>
+                        </div>
+                        <div class="col-sm-8">
+                          <input
+                            v-model="form.email"
+                            type="email"
+                            name="email"
+                            placeholder="Enter your email"
+                            class="form-control"
+                            :class="{ 'is-invalid': form.errors.has('email') }"
+                          />
+                          <has-error :form="form" field="email"></has-error>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="row">
+                        <div class="col-sm-3 text-right">
+                          <label for="phone"><b>Phone :</b></label>
+                        </div>
+                        <div class="col-sm-8">
+                          <input
+                            v-model="form.phone"
+                            type="text"
+                            name="phone"
+                            placeholder="Enter Phone number"
+                            class="form-control"
+                            :class="{ 'is-invalid': form.errors.has('phone') }"
+                          />
+                          <has-error :form="form" field="phone"></has-error>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="row">
+                        <div class="col-sm-3 text-right">
+                          <label for="dob"><b>Date of Birth :</b></label>
+                        </div>
+                        <div class="col-sm-8">
+                          <input
+                            v-model="form.dob"
+                            type="date"
+                            name="dob"
+                            class="form-control"
+                            :class="{ 'is-invalid': form.errors.has('dob') }"
+                          />
+                          <has-error :form="form" field="dob"></has-error>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <div class="row">
+                        <div class="col-sm-3 text-right">
+                          <label for="address"><b>Address :</b></label>
+                        </div>
+                        <div class="col-sm-8">
+                          <input
+                            v-model="form.address"
+                            type="text"
+                            name="address"
+                            placeholder="Enter Address"
+                            class="form-control"
+                            :class="{ 'is-invalid': form.errors.has('address') }"
+                          />
+                          <has-error :form="form" field="address"></has-error>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+                <button :disabled="form.busy" type="submit" class="btn btn-primary w-100 mt-3">
+                  Update Profile
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+       <!-- Modal -->
+      <div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-edit"></i> <b>Change Profile Image!</b></h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body px-4">
+             <div class="text-center">
+               <img :src="avatar" alt="profile-image" width="150px"> <br>
+               <input type="file" id="profile" name="profile" @change="getImage" style="display:none;">
+               <label for="profile" class="btn btn-success btn-sm mt-2">Choose Image</label>
+               <button class="btn btn-primary btn-sm ml-2" @click.prevent="upload"><i class="fas fa-upload"></i> Upload</button>
+             </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -200,9 +223,12 @@
 import {Form} from 'vform';
 import $ from 'jquery';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 export default {
   data() {
     return {
+      image: '',
+      avatar: 'img/user.png',
       user_id:null,
       isLoggedIn: true,
       address: '',
@@ -211,7 +237,6 @@ export default {
       form: new Form({
         name: "",
         email: "",
-        porfile: "",
         phone: "",
         dob: "",
         address: "",
@@ -235,6 +260,47 @@ export default {
       axios.get('http://127.0.0.1:8000/api/userpost/'+ id) . then ( (response) => {
         this.postByUser = response.data;
       })
+    },
+    deletePost(id){
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        this.form.delete('http://127.0.0.1:8000/api/post/'+id).then(()=>{
+          this.getPostUser();
+          if (result.isConfirmed) {
+            Swal.fire(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        }).catch(() => {
+          Swal("Failed!","There was something wrongs.","warning");
+        })
+      })
+    },
+    getImage(e){
+      this.image = e.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(this.image);
+      reader.onload = e => {
+        this.avatar = e.target.result
+      }
+    },
+    upload( id = this.user_id){
+      let fd = new FormData();
+      fd.append('img',this.image);
+      axios.post('http://127.0.0.1:8000/api/changeprofile/'+ id ) . then( response => {
+        console.log(response.data)
+      }).catch(errs => {
+        console.log(errs.data);
+      }) 
     }
   },
   mounted() {
